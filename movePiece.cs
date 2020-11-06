a598b2d630ee04cf7c042f9dfdabf6bff3c420eb
Benjamin-scha <72273853+Benjamin-scha@users.noreply.github.com>
2020-11-06 14:24:51 -0600

    Add files via upload

---
+++ movePiece.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePiece : MonoBehaviour
+{
+    public float scaleFactor;
+    public float x;
+    public float up;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(x*scaleFactor, up, y*scaleFactor);
+        lockIntoSpace();
+    }
+
+
+
+
+
+    void lockIntoSpace()
+    {
+        x = Mathf.Ceil(x);
+        y = Mathf.Ceil(y);
+    }
+
+
+    void MoveToSpace()
+    {
+
+    }
+
+
+}
